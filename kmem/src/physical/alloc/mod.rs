618b34b14de0ecbd489496f4f186c8b0db422001
Fabian Thorand <f.thorand@gmail.com>
2018-12-26 14:22:14 +0100

    Remove null allocators

--- kmem/src/physical/alloc/mod.rs
+++ kmem/src/physical/alloc/mod.rs
@@ -1,10 +1,8 @@
 mod bump;
-mod null;
 
 use crate::physical::{PageFrame};
 
 pub use self::bump::BumpAllocator;
-pub use self::null::{NullAllocator, PanickingNullAllocator};
 
 /// Generic interface for a page frame allocator.
 pub trait PageFrameAllocator {
